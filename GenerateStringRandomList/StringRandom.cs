b3d30029c9803ef5594fdb5c3d87797aa0dcfe0f
PRONCHAI AKSONNITI <zpronchai.a@pttdigital.com>
2018-07-20 09:46:35 +0700

    Add project files.

---
+++ GenerateStringRandomList/StringRandom.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenerateStringRandomList
+{
+    public class StringRandom
+    {
+        private List<string> lstString;
+        public List<string> Values
+        {
+            get
+            {
+                return lstString;
+            }
+            set
+            {
+                lstString = value;
+            }
+        }
+
+        public long Count
+        {
+            get
+            {
+                return Values.Count;
+
+            }
+        }
+
+        public StringRandom()
+        {
+
+        }
+
+    }
+}
